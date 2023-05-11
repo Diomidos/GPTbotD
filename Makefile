b9291bc31d119301ebfbef03dca56706284d1925
[React-Hooks] <[ruslanprivozin277@gmail.com]>
2023-05-11 19:04:47 +0300

    first commit

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	docker build -t tmkomek .
+	docker build -t diomidos/boty .
 
 run:
-	docker run -d -p 3000:3000 --name tmkomek --rm tmkomek
+	docker run -d -p 3000:3000 --name diomidos/boty --rm diomidos/boty
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-import { unlink } from 'fs/promises'
+import { unlink } from "fs/promises";
 
-export const removeFile = async path => {
-	try {
-		await unlink(path)
-	} catch (error) {
-		console.log('Error while removing file', error.message)
-	}
-}
+export const removeFile = async (path) => {
+  try {
+    await unlink(path);
+  } catch (error) {
+    console.log("Error while removing file", error.message);
+  }
+};
